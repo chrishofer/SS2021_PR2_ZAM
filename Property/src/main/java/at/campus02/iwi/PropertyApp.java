bae9a6aac70a0a097862b3aca5c4741a75ca4e16
Christian Hofer <christian.hofer@campus02.at>
2021-04-28 10:10:24 +0200

    Updated testing

--- Property/src/main/java/at/campus02/iwi/PropertyApp.java
+++ Property/src/main/java/at/campus02/iwi/PropertyApp.java
@@ -6,5 +6,13 @@ public class PropertyApp {
         RentedFlat rf = new RentedFlat("Ms. Wrigly", 100, "Flat Street 12", true, 1, true);
         System.out.println(rf.upfrontCost());
         System.out.println(rf.monthlyCost());
+
+        Flat f = new Flat("Ms. Wrigly", 100, "Flat Street 15", false, 2);
+        System.out.println(f.upfrontCost());
+        System.out.println(f.monthlyCost());
+
+        House h = new House("Mr. Owner", 50, "House Lane 2", 2017);
+        System.out.println(h.upfrontCost());
+        System.out.println(h.monthlyCost());
     }
 }
